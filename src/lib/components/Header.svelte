3b01371c3c70711aa6031ef5c805ee9a8b05d5e3
Lucas R. Marques <sagnax@gmail.com>
2024-01-03 13:53:40 -0400

    working on nav bar

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,36 @@
+<header
+  class="flex px-4 items-center h-[64px] py-2 bg-white z-10 justify-between sticky top-0 left-0"
+>
+  <div class="flex gap-4 items-center">
+    <img
+      src="./icons/menuIcon.png"
+      class="cursor-pointer p-3 rounded-3xl hover:bg-green-200 h-16 w-16"
+      alt=""
+    />
+    <img
+      src="./icons/ytLogo.png"
+      class="cursor-pointer p-3 rounded-3xl bg-green-100 hover:bg-green-200 h-16 w-16"
+      alt=""
+    />
+  </div>
+
+  <div>Search Bar</div>
+
+  <div class="flex gap-3 items-center">
+    <img
+      src="./icons/createIcon.png"
+      class="cursor-pointer p-3 rounded-3xl hover:bg-green-200 h-16 w-16"
+      alt=""
+    />
+    <img
+      src="./icons/notificationIcon.png"
+      class="cursor-pointer p-3 rounded-3xl hover:bg-green-200 h-16 w-16"
+      alt=""
+    />
+    <img
+      src="./icons/avatarIcon.png"
+      class="cursor-pointer p-3 rounded-3xl bg-green-100 hover:bg-green-200 h-16 w-16"
+      alt=""
+    />
+  </div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Header from "$lib/components/Header.svelte";
+    import "./style.css";
+</script>
+
+<Header />
+<slot />
